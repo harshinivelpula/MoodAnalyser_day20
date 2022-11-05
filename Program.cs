679e45a8a7f8b20ebdce4eb9356d61bcef2a25fd
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-05 15:50:17 +0530

    check mood

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Moodanalyser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Mood check = new Mood("msg");
+            Console.WriteLine(check.AnalyseMood("I am sad"));
+            Console.WriteLine(check.AnalyseMood("I am happy"));
+
+
+            Mood check1 = new Mood("msg");
+            check1.AnalyseAnyMood("I am in any mood");
+        }
+    }
+}
+
+
+
